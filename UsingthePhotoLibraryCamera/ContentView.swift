c512d41a6d243f89fac1cfe6201967dbca3dd7cf
neddington <neddington@cascadia.edu>
2023-12-11 14:45:22 -0800

    Fin

--- UsingthePhotoLibraryCamera/ContentView.swift
+++ UsingthePhotoLibraryCamera/ContentView.swift
@@ -8,17 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            HStack {
+                Button("Photo Library") {
+                    self.sourceType = .photoLibrary
+                    self.showingImagePicker.toggle()
+                }
+                .padding()
+                
+                Button("Camera") {
+                    self.sourceType = .camera
+                    self.showingImagePicker.toggle()
+                }
+                .padding()
+            }
         }
-        .padding()
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$image, sourceType: self.sourceType)
+        }
+    }
+    
+    func loadImage() {
+        guard let selectedImage = image else { return }
+        image = selectedImage
     }
 }
 
-#Preview {
-    ContentView()
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: Image?
+    @Environment(\.presentationMode) var presentationMode
+    var sourceType: UIImagePickerController.SourceType
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = Image(uiImage: uiImage)
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
