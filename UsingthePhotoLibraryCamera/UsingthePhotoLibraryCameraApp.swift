8459df8bad3830d75b9f1b61f668c25adb39d456
neddington <neddington@cascadia.edu>
2023-12-11 14:41:44 -0800

    Initial Commit

---
+++ UsingthePhotoLibraryCamera/UsingthePhotoLibraryCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingthePhotoLibraryCameraApp.swift
+//  UsingthePhotoLibraryCamera
+//
+//  Created by Eddington, Nick on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UsingthePhotoLibraryCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
